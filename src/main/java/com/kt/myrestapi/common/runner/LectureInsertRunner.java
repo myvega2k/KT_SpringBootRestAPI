0be4fdb993b68544d8876431df1dd5d4c36de8db
myvega2k <vega2k@kakao.com>
2024-04-30 10:14:02 +0900

    InsertRunner update

--- src/main/java/com/kt/myrestapi/common/runner/LectureInsertRunner.java
+++ src/main/java/com/kt/myrestapi/common/runner/LectureInsertRunner.java
@@ -18,11 +18,13 @@ public class LectureInsertRunner implements ApplicationRunner {
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		IntStream.range(0, 15).forEach(this::generateLecture);
+		IntStream.rangeClosed(1, 15)
+                //.forEach(value -> generateLecture(value));
+                .forEach(this::generateLecture);
 	}
-    private Lecture generateLecture(int index) {
+    private void generateLecture(int index) {
         Lecture lecture = buildLecture(index);
-        return this.lectureRepository.save(lecture);
+        this.lectureRepository.save(lecture);
     }
     private Lecture buildLecture(int index) {
         return Lecture.builder()
