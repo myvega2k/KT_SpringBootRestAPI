51b7d99d022dc6353459fc40823216f51d6316cb
myvega2k <vega2k@kakao.com>
2024-04-30 17:46:07 +0900

    advice update

--- src/main/java/com/kt/myrestapi/common/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/kt/myrestapi/common/exception/advice/DefaultExceptionAdvice.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -56,6 +58,18 @@ public class DefaultExceptionAdvice {
         result.put("httpStatus", HttpStatus.BAD_REQUEST.value());
 
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+    //403 예외 발생
+    @ExceptionHandler(value = AccessDeniedException.class)
+    public void accessDeniedExceptionHandler(Exception e) {
+        throw new AccessDeniedException(e.getMessage());
+    }
+
+    //401 예외 발생
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public void badCredentialExceptionHandler(BadCredentialsException e){
+        throw new BadCredentialsException(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
