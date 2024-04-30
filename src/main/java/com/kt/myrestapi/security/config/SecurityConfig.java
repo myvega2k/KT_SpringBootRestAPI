0b4af099a4a885130a1e919ad4433e81a25824b3
myvega2k <vega2k@kakao.com>
2024-04-30 17:49:35 +0900

    advice update

--- src/main/java/com/kt/myrestapi/security/config/SecurityConfig.java
+++ src/main/java/com/kt/myrestapi/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.kt.myrestapi.security.config;
 
+import com.kt.myrestapi.security.exception.CustomAccessDeniedHandler;
+import com.kt.myrestapi.security.exception.CustomAuthenticationEntryPoint;
 import com.kt.myrestapi.security.filter.JwtAuthenticationFilter;
 import com.kt.myrestapi.security.userinfo.UserInfoUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +21,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -46,7 +50,20 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(authManager -> authManager
+                        .authenticationEntryPoint(authenticationEntryPoint())
+                        .accessDeniedHandler(accessDeniedHandler()))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new CustomAuthenticationEntryPoint();
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
     }
 
     @Bean
