a1d84d25f59ac7d8bc04fc6b573642e0b60de4c6
myvega2k <vega2k@kakao.com>
2024-04-30 13:20:42 +0900

    probledetail add

--- src/main/java/com/kt/myrestapi/common/exception/advice/DefaultExceptionAdvice.java
+++ src/main/java/com/kt/myrestapi/common/exception/advice/DefaultExceptionAdvice.java
@@ -5,11 +5,13 @@ import com.kt.myrestapi.common.exception.SystemException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,13 +20,23 @@ public class DefaultExceptionAdvice {
 	private final Logger LOGGER = LoggerFactory.getLogger(DefaultExceptionAdvice.class);
 
 
-    @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<Object> handleException(BusinessException e) {
-        Map<String, Object> result = new HashMap<String, Object>();
-        result.put("message", "[안내] " + e.getMessage());
-        result.put("httpStatus", e.getHttpStatus().value());
+//    @ExceptionHandler(BusinessException.class)
+//    protected ResponseEntity<Object> handleException(BusinessException e) {
+//        Map<String, Object> result = new HashMap<String, Object>();
+//        result.put("message", "[안내] " + e.getMessage());
+//        result.put("httpStatus", e.getHttpStatus().value());
+//
+//        return new ResponseEntity<>(result, e.getHttpStatus());
+//    }
 
-        return new ResponseEntity<>(result, e.getHttpStatus());
+    @ExceptionHandler(BusinessException.class)
+    protected ProblemDetail handleException(BusinessException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(e.getHttpStatus());
+        problemDetail.setTitle("Not Found");
+        problemDetail.setDetail(e.getMessage());
+        problemDetail.setProperty("errorCategory", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
     }
     
     @ExceptionHandler(SystemException.class)
