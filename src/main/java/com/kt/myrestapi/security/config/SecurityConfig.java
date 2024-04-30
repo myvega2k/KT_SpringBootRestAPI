fe04067d6cfd147530ad8be2e2b02de0581f5f90
myvega2k <vega2k@kakao.com>
2024-04-30 14:32:58 +0900

    security config update

--- src/main/java/com/kt/myrestapi/security/config/SecurityConfig.java
+++ src/main/java/com/kt/myrestapi/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kt.myrestapi.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +23,18 @@ public class SecurityConfig {
     }
 
     @Bean
-//authentication
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/users/welcome").permitAll()
+                            .requestMatchers("/api/lectures/**").authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+
+    @Bean
+    //authentication
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
         UserDetails admin = User.withUsername("adminboot")
                 .password(encoder.encode("pwd1"))
@@ -31,4 +46,6 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
     }
+
+
 }
