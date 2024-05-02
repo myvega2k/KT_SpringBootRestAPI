296373a129532df5c6d122ac38a6ec18c9cdd418
myvega2k <vega2k@kakao.com>
2024-05-03 01:56:38 +0900

    configuration-processor dependency 제거, Jackson ObjectMapper 사용

--- src/main/java/com/kt/myrestapi/security/exception/CustomAccessDeniedHandler.java
+++ src/main/java/com/kt/myrestapi/security/exception/CustomAccessDeniedHandler.java
@@ -1,11 +1,10 @@
 package com.kt.myrestapi.security.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -24,18 +23,15 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
         // Set response code
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-        // Create response content
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("code", HttpServletResponse.SC_FORBIDDEN);
-            jsonObject.put("message", "요청된 리소스에 대한 접근 권한이 없습니다!");
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpServletResponse.SC_FORBIDDEN);
+        errorObject.setMessage("요청된 리소스에 대한 유효한 권한이 없습니다!!");
 
         // Add content to the response
-        log.error(accessDeniedException.getMessage(), accessDeniedException);
-        response.getWriter().write(jsonObject.toString());
+        String json = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(errorObject);
+        response.getWriter().write(json);
 
     }
 }
--- src/main/java/com/kt/myrestapi/security/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/kt/myrestapi/security/exception/CustomAuthenticationEntryPoint.java
@@ -1,11 +1,10 @@
 package com.kt.myrestapi.security.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -18,26 +17,24 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest req,
-                         HttpServletResponse res,
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
                          AuthenticationException authException)
             throws IOException, ServletException {
 
-        res.setContentType("application/json;charset=UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         //Set response status code 401
-        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
+        ObjectMapper mapper = new ObjectMapper();
         // Create response content
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("code", HttpServletResponse.SC_UNAUTHORIZED);
-            jsonObject.put("message", "요청된 리소스에 대한 유효한 인증 자격 증명이 없습니다!");
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpServletResponse.SC_UNAUTHORIZED);
+        errorObject.setMessage("요청된 리소스에 대한 유효한 인증 자격 증명이 실패 하였습니다!!");
 
-        log.error(authException.getMessage(), authException);
-        res.getWriter().write(jsonObject.toString());
+        String json = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(errorObject);
+        response.getWriter().write(json);
 
     }
 }
