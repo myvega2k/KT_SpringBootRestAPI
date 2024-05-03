6e5933131303dcb1f9246e5657f30fc185c8726d
myvega2k <vega2k@kakao.com>
2024-05-03 17:25:19 +0900

    updateLecture 등록한Uesr와수정을요청한User가다르면401에러,Lecture와연관된User가있으면email정보설정

--- src/main/java/com/kt/myrestapi/lectures/LectureController.java
+++ src/main/java/com/kt/myrestapi/lectures/LectureController.java
@@ -6,6 +6,8 @@ import com.kt.myrestapi.lectures.dto.LectureReqDto;
 import com.kt.myrestapi.lectures.dto.LectureResDto;
 import com.kt.myrestapi.lectures.dto.LectureResource;
 import com.kt.myrestapi.lectures.validator.LectureValidator;
+import com.kt.myrestapi.security.userinfo.UserInfo;
+import com.kt.myrestapi.security.userinfo.annot.CurrentUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -19,6 +21,7 @@ import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,7 +46,8 @@ public class LectureController {
     @PutMapping("/{id}")
     public ResponseEntity updateLecture(@PathVariable Integer id,
                                         @RequestBody @Valid LectureReqDto lectureReqDto,
-                                        Errors errors) {
+                                        Errors errors,
+                                        @CurrentUser UserInfo currentUser) {
         Lecture existingLecture = getExistingLecture(id);
 
         if (errors.hasErrors()) {
@@ -55,10 +59,20 @@ public class LectureController {
             return getErrors(errors);
         }
 
+        //Lecture가 참조하는 UserInfo 객체와 인증한 UserInfo 객체가 다르면 401 인증 오류
+        if((existingLecture.getUserInfo() != null) && (!existingLecture.getUserInfo().equals(currentUser))) {
+            throw new BadCredentialsException("등록한 User와 수정을 요청한 User가 다릅니다.");
+            //return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+
         this.modelMapper.map(lectureReqDto, existingLecture);
         existingLecture.update();
         Lecture savedLecture = this.lectureRepository.save(existingLecture);
         LectureResDto lectureResDto = modelMapper.map(savedLecture, LectureResDto.class);
+
+        //Lecture 객체와 연관된 UserInfo 객체가 있다면 LectureResDto에 email을 set
+        if(savedLecture.getUserInfo() != null)
+            lectureResDto.setEmail(savedLecture.getUserInfo().getEmail());
 
         LectureResource lectureResource = new LectureResource(lectureResDto);
         return ResponseEntity.ok(lectureResource);
